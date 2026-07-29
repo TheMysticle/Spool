e8ff86a0c28b7b1867ee16dd3e2776728e2b31d6
Oliwier Krawczyk <36317711+TheMysticle@users.noreply.github.com>
2026-07-29 17:25:53 +0200

    style: aggressively pull video grid up to match top spacing on mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1665,7 +1665,7 @@
             </div>
           </div>
         </div>
-        <div id="channel-page-content">
+        <div id="channel-page-content" style="margin-top: -16px;">
           <div class="state-loading"><div class="spinner"></div></div>
         </div>
       `;
