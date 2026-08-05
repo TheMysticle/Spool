8ee40bda2fb81debbb2bcf7c389574df064abf3e
Oliwier Krawczyk <36317711+TheMysticle@users.noreply.github.com>
2026-08-05 02:24:28 +0200

    style: move mobile desc above subscribe button in info bar

--- public/js/main.js
+++ public/js/main.js
@@ -1861,22 +1861,22 @@
                 })() : ''}
               </div>
             </div>
+            ${ch.description ? (() => {
+              const fullDesc = escHtml(ch.description);
+              const safeDescForClick = fullDesc.replace(/'/g, '&#39;').replace(/(\r\n|\n|\r)/gm, '&#10;');
+              const mobileLimit = 85;
+              const mobileTrunc = fullDesc.length > mobileLimit ? fullDesc.substring(0, mobileLimit) + '... ' : fullDesc;
+              return `
+                <div class="channel-page-mobile-desc-bar" onclick="window.openChannelDescriptionModal('${safeDescForClick}')" style="cursor: pointer;">
+                  ${mobileTrunc}${fullDesc.length > mobileLimit ? '<span class="desc-more">more</span>' : ''}
+                </div>
+              `;
+            })() : ''}
             <div class="channel-page-actions">
               <button class="btn ${subButtonClass}" onclick="toggleChannelSubscription('${id}', this)">${subButtonText}</button>
               ${canEdit ? `<button class="vhs-settings-btn" onclick="setupVhsPassword('${id}')" title="VHS Settings"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg></button>` : ''}
             </div>
           </div>
-          ${ch.description ? (() => {
-            const fullDesc = escHtml(ch.description);
-            const safeDescForClick = fullDesc.replace(/'/g, '&#39;').replace(/(\r\n|\n|\r)/gm, '&#10;');
-            const mobileLimit = 85;
-            const mobileTrunc = fullDesc.length > mobileLimit ? fullDesc.substring(0, mobileLimit) + '... ' : fullDesc;
-            return `
-              <div class="channel-page-mobile-desc-bar" onclick="window.openChannelDescriptionModal('${safeDescForClick}')" style="cursor: pointer;">
-                ${mobileTrunc}${fullDesc.length > mobileLimit ? '<span class="desc-more">more</span>' : ''}
-              </div>
-            `;
-          })() : ''}
           
           <div class="channel-page-nav">
             <div class="channel-page-tabs">
